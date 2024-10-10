1284247c148871054bd320545e7a4b0e5930405c
1spyral <83098914+1spyral@users.noreply.github.com>
2024-10-10 19:22:38 -0400

    Triangle Problems

---
+++ bonus.py
@@ -0,0 +1,2 @@
+# Assumptions: matplotlib is installed! pip install matplotlib
+import matplotlib.pyplot as plt; plt.fill([0, 0, int(input("Height: "))], (0, int(input("Width: ")), 0)) and plt.show()
---
+++ triangle.py
@@ -0,0 +1,2 @@
+# Assumptions: matplotlib is installed! pip install matplotlib
+import matplotlib.pyplot as plt; plt.fill([0, 0, 4], (0, 3, 0)) and plt.show()
